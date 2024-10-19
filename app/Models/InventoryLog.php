ee7df070b8c68f71841146728850bbc3135d94ae
BAQAR RAZA MANGRANI <baqarmangrani@gmail.com>
2024-10-19 12:31:40 -0400

    missing     use HasFactory;

--- app/Models/InventoryLog.php
+++ app/Models/InventoryLog.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class InventoryLog extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['product_id', 'quantity_change', 'type', 'comments'];
 
     public function product()
