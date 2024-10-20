19a508010581e74632e9d69d56be73e43d5170f9
BAQAR RAZA MANGRANI <baqarmangrani@gmail.com>
2024-10-19 20:36:05 -0400

    format

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@ Route::get('/', function () {
 
 Route::resource('products', ProductController::class);
 
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
